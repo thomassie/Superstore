3138e5ffd5755db14a8297acd404c546634ef026
Thomas <thomasmmassie@googlemail.com>
2020-02-07 10:15:01 +0100

    minor updates

--- Analysis/Sample EU Superstore --- Analysis.R
+++ Analysis/Sample EU Superstore --- Analysis.R
@@ -8,6 +8,9 @@ library(readxl)
 library(skimr)
 library(ggExtra)
 library(GGally)
+library(lubridate)
+library(gganimate)
+library(plotly)
 
 dd_org_url <- "https://raw.githubusercontent.com/thomassie/Superstore/master/Data/Superstore/Sample%20-%20EU%20Superstore.csv"
 dd_org <- read.csv2(dd_org_url, stringsAsFactors = FALSE)
@@ -20,20 +23,24 @@ head(dd_org)
 
 dd <- dd_org %>% 
   mutate_at(c("Order.Date", "Ship.Date"), as.Date, format = "%d.%m.%y") %>% 
-  mutate_at(c("Sales", "Quantity", "Discount", "Profit"), as.numeric)
+  mutate_at(c("Sales", "Quantity", "Discount", "Profit"), as.numeric) %>% 
+  mutate(year_Order = year(Order.Date))
 
-ggpairs(dd_org, columns = c("Country", "Category", "Quantity", "Sales", "Profit"))
+ggpairs(dd, columns = c("Country", "Category", "Quantity", "Sales", "Profit"))
 
-p1 <- dd_org %>% 
-  filter(., Country == "Switzerland") %>% 
+p1 <- dd %>% 
+  # filter(., Country == "Switzerland") %>% 
   ggplot(.,
-         aes(x = Quantity, y = Profit)) +
+         aes(x = Sales, 
+             y = Profit,
+             colour = Country)) +
   geom_point() +
   theme_minimal()
 
 p1
 
-ggMarginal(p1, type = "histogram")
+ggMarginal(p1, type = "density")
+
 
 
 
