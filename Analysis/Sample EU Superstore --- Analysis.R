5e70f775815aec60e6fbfaba80dcbe69e9ddda3c
Thomas <thomasmmassie@googlemail.com>
2020-03-10 21:23:11 +0100

    Update Sample EU Superstore --- Analysis.R

--- Analysis/Sample EU Superstore --- Analysis.R
+++ Analysis/Sample EU Superstore --- Analysis.R
@@ -6,11 +6,18 @@ library(RCurl)
 library(tidyverse)
 library(readxl)
 library(skimr)
+library(forecats)
 library(ggExtra)
 library(GGally)
 library(lubridate)
 library(gganimate)
 library(plotly)
+library(RColorBrewer)
+library(wesanderson)
+
+
+
+###########################################
 
 dd_org_url <- "https://raw.githubusercontent.com/thomassie/Superstore/master/Data/Superstore/Sample%20-%20EU%20Superstore.csv"
 dd_org <- read.csv2(dd_org_url, stringsAsFactors = FALSE)
@@ -21,25 +28,66 @@ skim(dd_org)
 head(dd_org)
 
 
+
+###########################################
+
 dd <- dd_org %>% 
   mutate_at(c("Order.Date", "Ship.Date"), as.Date, format = "%d.%m.%y") %>% 
   mutate_at(c("Sales", "Quantity", "Discount", "Profit"), as.numeric) %>% 
-  mutate(year_Order = year(Order.Date))
+  mutate_at(c("Ship.Mode", "Segment", "City", "State", "Country", "Region", "Category", "Sub.Category", "Customer.ID", "Product.ID", "Product.Name"), as.factor) %>% 
+  mutate(Year_Order = as.factor(year(Order.Date)))
 
 ggpairs(dd, columns = c("Country", "Category", "Quantity", "Sales", "Profit"))
 
+
+
+
+
+###########################################
+
 p1 <- dd %>% 
-  # filter(., Country == "Switzerland") %>% 
+  # filter(., Country == "Switzerland") %>%
   ggplot(.,
          aes(x = Sales, 
              y = Profit,
-             colour = Country)) +
-  geom_point() +
-  theme_minimal()
+             colour = Year_Order)) +
+  geom_point(alpha = 0.6) +
+  # scale_x_log10() +
+  # scale_color_brewer(palette = "Paired") +
+  scale_colour_manual(values = wes_palette("Darjeeling1")) +
+  theme_minimal() +
+  theme(legend.position = "none")
 
 p1
 
-ggMarginal(p1, type = "density")
+ggMarginal(p1, type = "density", fill = "#999999", colour = "#999999")
+
+
+
+###########################################
+
+
+
+dd_sum <- dd %>% 
+  group_by(Year_Order, Region, Country) %>% 
+  summarise(Sales_sum = sum(Sales),
+            Profit_sum = sum(Profit))
+
+dd_viz <- dd_sum %>% 
+  
+
+
+dd %>% 
+  ggplot(aes(x = Country, y = Sales)) +
+  geom_tile() +
+  coord_flip()
+
+staticplot = ggplot(dd, aes(rank, group = C, 
+                                       fill = as.factor(country_name), color = as.factor(country_name))) +
+  geom_tile(aes(y = value/2,
+                height = value,
+                width = 0.9), alpha = 0.8, color = NA) +
+  geom_text(aes(y = 0, label = paste(country_name, " ")), vjust = 0.2, hjust = 1) +
 
 
 
